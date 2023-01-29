1f86abddbbb9ae216aada604f20fbb4fc30e1835
Gariboyina-Akhil <akhilgariboina@gmail.com>
2023-01-29 12:47:30 +0530

    added mongoose model

---
+++ database/book.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+//schema
+const BookSchema = mongoose.Schema({
+    ISBN : String,
+    name : String,
+    Authors: [Number],
+    language: String,
+    pubDate : String,
+    noOfPages : Number,
+    category:[String],
+    publications : Number
+});
+
+//model
+const BookModel = mongoose.model(BookSchema);
+
+module.exports = BookModel ;
